b5092324bfc4698106d6d6e90ec19a75d2756f23
Thiago Willecke <thiagogwillecke@gmail.com>
2023-08-09 14:20:56 -0300

    refactor app component with composition api

--- client/src/AppOptionsAPI.vue
+++ client/src/AppOptionsAPI.vue
@@ -4,33 +4,32 @@ import axios from "axios"
 export default {
   data() {
     return {
-      board: {
-        name: "Project A",
-        columns: [
-          {
-            name: "Todo",
-            cards: [
-              { title: "A", estimative: 3 },
-              { title: "B", estimative: 2 },
-              { title: "C", estimative: 1 }
-            ]
-          },
-          {
-            name: "Doing",
-            cards: []
-          },
-          {
-            name: "Done",
-            cards: []
-          },
-        ]
-      },
-      columnName: ""
+      board: undefined,
+      columnName: "",
+      cardTitle: ""
     }
   },
   methods: {
     addColumn(columnName) {
       this.board.columns.push({ name: columnName, cards: [] });
+    },
+    addCard(column, cardTitle) {
+      column.cards.push({ title: cardTitle, estimative: 3 });
+      column.estimative += 3;
+    },
+    increaseEstimative(card) {
+      card.estimative++;
+    }
+  },
+  computed: {
+    boardEstimative() {
+      return this.board.columns.reduce((total, column) => {
+        total += column.cards.reduce((total, card) => {
+          total += card.estimative;
+          return total;
+        }, 0);
+        return total;
+      }, 0);
     }
   },
   async mounted() {
@@ -44,12 +43,18 @@ export default {
 </script>
 
 <template>
-  <h3>{{ board.name }} {{ board.estimative }}</h3>
+  <h3>{{ board.name }} {{ boardEstimative }}</h3>
   <div class="columns">
     <div class="column" v-for="column in board.columns">
       <h3>{{ column.name }} {{ column.estimative }}</h3>
       <div class="card" v-for="card in column.cards">
-        {{ card.title }}
+        {{ card.title }} {{ card.estimative }}
+        <br />
+        <button @click="increaseEstimative(card)">+</button><button>-</button>
+      </div>
+      <div class="card new-card">
+        <input type="text" v-model="cardTitle" />
+        <button v-on:click="addCard(column, cardTitle)">Add</button>
       </div>
     </div>
     <div class="column new-column">
@@ -91,5 +96,11 @@ export default {
   display: flex;
   align-items: center;
   justify-content: space-around;
+}
+
+.new-card {
+  background-color: #EEE;
+  border: 1px dashed #CCC;
+  display: block;
 }
 </style>
