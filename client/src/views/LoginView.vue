4e0d4d4c826f793de77db5e6fe9cb71b02231f16
Thiago Willecke <thiagogwillecke@gmail.com>
2023-10-17 14:01:33 -0300

    Add login view

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const username = ref("");
+const password = ref("");
+
+
+function login(username: string, password: string) {
+    console.log("login");
+}
+
+</script>
+
+<template>
+    <input type="text" v-model="username" />
+    <input type="password" v-model="password">
+    <button @click="login(username, password)">Login</button>
+</template>
